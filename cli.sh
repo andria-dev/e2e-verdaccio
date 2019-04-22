df95214648bf2c72a87bd4fe7ec3e6e30884e655
Christopher Brown <chbphone55@gmail.com>
2019-04-21 19:14:32 -0600

    works flawlessly

--- cli.sh
+++ cli.sh
@@ -3,6 +3,15 @@
 set -e
 
 local_registry=${LOCAL_REGISTRY_URL:-"http://localhost:4873"}
+if [ ! -z $PACKAGE_RUNNER ]; then
+  echo "Using $PACKAGE_RUNNER from env variable"
+  package_runner=$PACKAGE_RUNNER
+elif [ -f pnpm-lock.yaml ]; then
+  echo "Using $PACKAGE_RUNNER from pnpm-lock.yaml"
+  package_runner="pnpx"
+else
+  package_runner="npx"
+fi
 
 # Start local registry
 tmp_registry_log=`mktemp`
@@ -12,13 +21,14 @@ bash -c "nohup verdaccio &>$tmp_registry_log &"
 grep -q 'http address' <(tail -f $tmp_registry_log)
 
 # Login so that we can "publish"
-bash -c "npx npm-cli-login -u test -p test -e test@test.com -r $local_registry"
-
+bash -c "${package_runner} npm-cli-login -u test -p test -e test@test.com -r $local_registry"
 
 # Unpublish current version
 bash -c "npm unpublish -f --registry $local_registry"
 # Run publish command
 bash -c "npm publish --registry $local_registry"
 
+echo
 echo "Running the registry on http://localhost:4873"
 read -n 1 -s -r -p "Press any key to exit and close the verdaccio server"
+echo
